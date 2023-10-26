43ee0ce7ed1576665aa608f23193e3f20030f9da
Gabriela Gomez <ggaabrielagomez@gmail.com>
2023-10-26 12:49:27 -0600

    Update rules

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const auth = require('json-server-auth');
 const middlewares = jsonServer.defaults();
 const cors = require("cors");
 
 server.use(middlewares);
-server.use(router);
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
+const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
+
+server.use(rules);
 server.use(cors());
 server.listen(8080, () => {
   console.log("JSON Server is running");
