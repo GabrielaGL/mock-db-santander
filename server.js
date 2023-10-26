18d90b5b1f08304bc7c2237dbe352316cf2a7107
Gabriela Gomez <ggaabrielagomez@gmail.com>
2023-10-26 11:45:01 -0600

    update cors

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-const fs = require("fs");
-const path = require("path");
 const jsonServer = require("json-server");
-const auth = require("json-server-auth");
-const cors = require('cors');
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
+server.use(middlewares);
+server.use(router);
 
-const app = jsonServer.create();
-app.use(cors());
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-
-const port = process.env.PORT || 8080;
-
-
-// /!\ Bind the router db to the app
-app.db = router.db;
-
-
-app.use(middlewares);
-app.use(router);
-app.listen(port, () => {
-  console.log(`JSON Server is running in ${port}`);
+server.use(cors());
+server.listen(8080, () => {
+  console.log("JSON Server is running");
 });
